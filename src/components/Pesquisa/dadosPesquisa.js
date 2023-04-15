9b0ab1ecac6848e87f015edc3c1b86cce0c4fb3c
Mariane Cristina <marianedemaria@gmail.com>
2023-04-15 10:32:36 -0300

    first commit

---
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'JavaScript Assertivo', id: 2, src:livro},
+    {nome: 'Gestão de Negócios', id: 3, src:livro},
+    {nome: 'Aventureiros do Java', id: 4, src:livro},
+]
+
